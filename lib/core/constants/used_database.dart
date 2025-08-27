7eaecee5621a1f0f6da280f11a18b7b36a0befaa
faosfaos <turkmen_osman@yahoo.com>
2025-08-27 23:49:31 +0300

    _

--- lib/core/constants/used_database.dart
+++ lib/core/constants/used_database.dart
@@ -1,7 +1,11 @@
+/// Dependency injection ve data layer bileşenlerinin importları
 import '../../app/app.locator.dart';
 import '../../data/datasource/todo_repository_impl.dart';
 import '../../services/sql_database_service.dart';
 
+/// Database abstraction layer konfigürasyonu
+/// Repository pattern implementasyonu ile SQL database service'inin
+/// dependency injection container üzerinden inject edildiği singleton instance
 final usedDatabase = TodoRepositoryImpl(
   sqlDatabaseService: locator<SqlDatabaseService>(),
 );
